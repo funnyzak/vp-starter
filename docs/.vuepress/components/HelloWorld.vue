efc33d6523017b21b25da5e078db1aa70e4a10c3
leon <silenceace@gmail.com>
2022-11-16 22:10:47 +0800

    chore: code

--- docs/.vuepress/components/HelloWorld.vue
+++ docs/.vuepress/components/HelloWorld.vue
@@ -1,15 +1,14 @@
 <script setup lang="ts">
 console.log('hello world!')
 </script>
+<template>
+  <div class="hello-world">
+    <h1>Hello World!</h1>
+  </div>
+</template>
 
 <style scoped>
 .hello-world{
   display: none;
 }
 </style>
-
-<template>
-  <div class="hello-world">
-    <h1>Hello World!</h1>
-  </div>
-</template>
