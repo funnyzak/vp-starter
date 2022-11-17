45f12a79e477a0bee19d29b813c3234477fc4f2c
leon <silenceace@gmail.com>
2022-11-17 15:16:42 +0800

    feat: layout

--- docs/.vuepress/client.ts
+++ docs/.vuepress/client.ts
@@ -1,10 +1,14 @@
 import { defineClientConfig } from '@vuepress/client'
 import { installComponent, rootComponentsGlobal } from './components'
+import Layout from './layouts/Layout.vue'
 
 export default defineClientConfig({
   enhance({ app, router, siteData }) {
     // 注册自定义全局组件
     installComponent(app)
+  },
+  layouts: {
+    Layout
   },
   setup() {},
   // 注册全局组件，放在根节点
---
+++ docs/.vuepress/layouts/Layout.vue
@@ -0,0 +1,18 @@
+<script setup>
+import ParentLayout from '@vuepress/theme-default/layouts/Layout.vue'
+</script>
+
+<template>
+  <ParentLayout>
+    <template #page-bottom>
+      <div class="footer">MIT Licensed | Copyright © 2022 Funnyzak</div>
+    </template>
+  </ParentLayout>
+</template>
+
+<style lang="css">
+.footer {
+  text-align: center;
+  display: none;
+}
+</style>
