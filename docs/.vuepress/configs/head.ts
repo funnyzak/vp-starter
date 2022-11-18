1ff40125041f0cd7a0fb46135f469bcb78631730
Leon <silenceace@gmail.com>
2022-11-19 01:39:32 +0800

    feat: head viewport

--- docs/.vuepress/configs/head.ts
+++ docs/.vuepress/configs/head.ts
@@ -49,5 +49,12 @@ export const head = (base: string): HeadConfig[] => [
       name: 'referrer',
       content: 'no-referrer'
     }
+  ],
+  [
+    'meta',
+    {
+      name: 'viewport',
+      content: 'width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no'
+    }
   ]
 ]
